370998f0f50ab31bf3f868932c1a703f0e301264
HaoyouDeng <deng_haoyou@qq.com>
2022-07-09 21:39:39 +0800

    change train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -11,3 +11,16 @@ do
                         --train_aug \
                         -g 0
 done
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx":
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s_not_warmup"
+    time python train.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        --stop_epoch 800 \
+                        --train_aug \
+                        --not_warmup
+                        -g 0
+done

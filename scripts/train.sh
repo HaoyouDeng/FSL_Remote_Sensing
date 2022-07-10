1edf384a285a535a4ae306814554bd1ca26dc458
HaoyouDeng <deng_haoyou@qq.com>
2022-07-10 08:53:37 +0800

    fix errors in relationnet

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 SHOT=1
 MODEL="ResNet10"
 
-for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx":
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
 do
     NAME=$METHOD"_"$MODEL"_"$SHOT"s"
     time python train.py --model $MODEL \
@@ -12,7 +12,7 @@ do
                         -g 0
 done
 
-for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx":
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
 do
     NAME=$METHOD"_"$MODEL"_"$SHOT"s_not_warmup"
     time python train.py --model $MODEL \

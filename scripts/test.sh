2fe8b3265e11d0c3ebfc1ebeefb0f924af510161
HaoyouDeng <deng_haoyou@qq.com>
2022-07-23 16:00:05 +0800

    add test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,14 @@
 SHOT=1
 MODEL="ResNet10"
 
-for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+for METHOD in "protonet" "relationnet" "maml_approx"
 do
     NAME=$METHOD"_"$MODEL"_"$SHOT"s"
     time python test.py --model $MODEL \
                         --method $METHOD \
                         --n_shot $SHOT \
                         --name $NAME \
-                        -g 0
+                        -g 3
 done
 
 for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
@@ -18,5 +18,166 @@ do
                         --method $METHOD \
                         --n_shot $SHOT \
                         --name $NAME \
-                        -g 0
+                        -g 3
+done
+
+SHOT=5
+MODEL="ResNet10"
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s_not_warmup"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+SHOT=1
+MODEL="ResNet12"
+
+for METHOD in "protonet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s_not_warmup"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+SHOT=5
+MODEL="ResNet12"
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s_not_warmup"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+SHOT=1
+MODEL="ResNet18"
+
+for METHOD in "protonet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s_not_warmup"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+SHOT=5
+MODEL="ResNet18"
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s_not_warmup"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+SHOT=1
+MODEL="Conv4"
+
+for METHOD in "protonet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s_not_warmup"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+SHOT=5
+MODEL="Conv4"
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
+done
+
+for METHOD in "protonet" "matchingnet" "relationnet" "maml_approx"
+do
+    NAME=$METHOD"_"$MODEL"_"$SHOT"s_not_warmup"
+    time python test.py --model $MODEL \
+                        --method $METHOD \
+                        --n_shot $SHOT \
+                        --name $NAME \
+                        -g 3
 done
